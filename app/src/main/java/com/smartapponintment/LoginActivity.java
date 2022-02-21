9721e49c34b0e196d3154aa20bf17d6f3ff2f9d6
AMAAN <amaansajina4580@mail.com>
2022-02-21 11:53:57 +0530

    Commit

--- app/src/main/java/com/smartapponintment/LoginActivity.java
+++ app/src/main/java/com/smartapponintment/LoginActivity.java
@@ -4,9 +4,11 @@ import androidx.activity.OnBackPressedDispatcherOwner;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +23,7 @@ public class LoginActivity extends AppCompatActivity {
     EditText edtEmail;
     EditText edtPassword;
     TextView newAcc;
+    TextView tvfp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,60 +35,90 @@ public class LoginActivity extends AppCompatActivity {
         edtPassword = findViewById(R.id.edt_password2);
         newAcc = findViewById(R.id.new_acc);
 
+        tvfp = findViewById(R.id.tv_fp);
+
+        tvfp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View fpView = layoutInflater.inflate(R.layout.raw_fp, null);
+                EditText edtFPEmail = fpView.findViewById(R.id.edt_email);
+                Button btnSubmit = fpView.findViewById(R.id.btn_submit);
+                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                AlertDialog alertDialog = builder.create();
+                alertDialog.setView(fpView);
+                alertDialog.show();
+
+                btnSubmit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(LoginActivity.this, "Submit", Toast.LENGTH_SHORT).show();
+
+                        if (alertDialog.isShowing()) {
+
+                            alertDialog.dismiss();
+                        }
+                    }
+                });
+            }
+        });
+
+
         edtLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String strEmail = edtEmail.getText().toString();
-                String strPassword = edtPassword.getText().toString();
+                    @Override
+                    public void onClick(View view) {
+                        String strEmail = edtEmail.getText().toString();
+                        String strPassword = edtPassword.getText().toString();
 
-                if (strEmail.equals("")) {
-                    Toast.makeText(LoginActivity.this, "Enter Email id", Toast.LENGTH_SHORT).show();
-                } else if (strPassword.equals("")) {
-                    Toast.makeText(LoginActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(LoginActivity.this, "Login Successful!!", Toast.LENGTH_SHORT).show();
-                }
+                        if (strEmail.equals("")) {
+                            Toast.makeText(LoginActivity.this, "Enter Email id", Toast.LENGTH_SHORT).show();
+                        } else if (strPassword.equals("")) {
+                            Toast.makeText(LoginActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Login Successful!!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+                newAcc.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent i = new Intent(LoginActivity.this, SignUpActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
+                });
             }
-        });
 
-        newAcc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i =new Intent (LoginActivity.this,SignUpActivity.class);
-                startActivity(i);
-                finish();
+            public void onBackPressed() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                builder.setTitle("eAppointment");
+                builder.setIcon(R.drawable.ic_home2);
+                builder.setMessage("Are you sure, you want to Back?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        dialog.dismiss();
+
+                    }
+                });
+
+
+                builder.show();
+
             }
-        });
-    }
-
-        public void onBackPressed ()
-        {
-            AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
-            builder.setTitle("eAppointment");
-            builder.setIcon(R.drawable.ic_home2);
-            builder.setMessage("Are you sure, you want to Back?");
-            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-
-                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(i);
-                    finish();
-                }
-            });
-            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-
-                    dialog.dismiss();
-
-                }
-            });
-
-
-            builder.show();
-
         }
-    }
+
 
