dd77ce8b6b63c0bb4c6c19d878ec49190ba8196e
AMAAN <amaansajina4580@mail.com>
2022-02-25 09:28:45 +0530

    Commit

--- app/src/main/java/com/smartapponintment/fragments/SettingFragment.java
+++ app/src/main/java/com/smartapponintment/fragments/SettingFragment.java
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.smartapponintment.R;
 import com.smartapponintment.activities.LoginActivity;
@@ -19,6 +20,10 @@ import com.smartapponintment.activities.MainActivity;
 
 
 public class SettingFragment extends Fragment {
+
+    Button btnLogout;
+
+
     @Nullable
     @Override
 
@@ -28,15 +33,20 @@ public class SettingFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View rootview = inflater.inflate(R.layout.fragment_setting,container,false);
+        btnLogout = rootview.findViewById(R.id.btn_login);
 
-
-        SharedPreferences sharedPreferences = getContext().getSharedPreferences("e_Appointment", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.remove("KEY_PREF_EMAIL");
-        editor.remove("KEY_PREF_Password");
-        editor.commit();
-
-
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences sharedPreferences = getContext().getSharedPreferences("e_Appointment", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.remove("KEY_PREF_EMAIL");
+                editor.remove("KEY_PREF_Password");
+                editor.commit();
+                Intent i = new Intent(getActivity(),LoginActivity.class);
+                startActivity(i);
+            }
+        });
         return rootview;
     }
 }
